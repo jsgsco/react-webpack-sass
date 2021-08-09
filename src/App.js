3fe098924927cd4dd7ff2d3aece4709de8f26604
Jhon Steven Saavedra <jhon.giron@uniminuto.edu.co>
2021-08-08 22:45:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss"
+
+function App() {
+    return (
+        <div className="app">
+            <h1>React + Webpack + SASS</h1>
+            <p>Mi primer proyecto de React con Webpack</p>
+        </div>
+    )
+}
+
+export default App
